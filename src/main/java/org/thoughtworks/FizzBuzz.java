9653288d58a4df246cad64979540a400ef01d8ae
Helena <helena.gomez@thoughtworks.com>
2022-12-02 17:51:13 +0100

    Add Fizz when number is multiple of three

--- src/main/java/org/thoughtworks/FizzBuzz.java
+++ src/main/java/org/thoughtworks/FizzBuzz.java
@@ -5,16 +5,24 @@ public class FizzBuzz {
 
     public String printNumbersFrom1To100() {
         var numbers = new StringBuilder();
+        var fizz = "Fizz";
 
         for (int i=1; i <= 100; i++) {
-            if (i==100)
-                numbers.append(i);
-            else
-                numbers.append(i).append("\n");
-
-            System.out.println(i);
+            if (checkMultipleThree(i)) {
+                if (i==100) {
+                    numbers.append(fizz);
+                } else {
+                    numbers.append(fizz).append("\n");
+                }
+            } else {
+                if (i==100) {
+                    numbers.append(i);
+                } else {
+                    numbers.append(i).append("\n");
+                }
+            }
         }
-
+        System.out.print(numbers);
         return numbers.toString();
     }
 
--- src/test/java/org/thoughtworks/FizzBuzzTest.java
+++ src/test/java/org/thoughtworks/FizzBuzzTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class FizzBuzzTest {
 
@@ -34,10 +34,33 @@ class FizzBuzzTest {
     }
 
     @Test
-    void shouldCheckMultiple3() {
+    void shouldIfCheckMultiple3() {
         int number = 3;
         var actual = fizzBuzz.checkMultipleThree(number);
         assertEquals(true, actual);
     }
+
+    @Test
+    void shouldCheckIfNotMultiple3() {
+        int number = 4;
+        var actual = fizzBuzz.checkMultipleThree(number);
+        assertEquals(false, actual);
+    }
+
+    @Test
+    void shouldPrintNumbers1to100ReplacingMultiplesOf3ByFizz() {
+        var actual = fizzBuzz.printNumbersFrom1To100();
+        assertTrue(actual.contains("Fizz"));
+        assertFalse(actual.contains("\n3\n"));
+        assertFalse(actual.contains("\n6\n"));
+        assertFalse(actual.contains("\n9\n"));
+        assertFalse(actual.contains("\n12\n"));
+        assertFalse(actual.contains("\n15\n"));
+        assertEquals(outputStreamCaptor.toString().trim(), actual);
+    }
+
+
+
+
 
 }
