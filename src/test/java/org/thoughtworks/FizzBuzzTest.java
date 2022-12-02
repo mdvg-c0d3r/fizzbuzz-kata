707ee218e25d3f6df41144ae5caaffdff3c18098
Manuel Vicent <manueldavid.v@thoughtworks.com>
2022-12-02 16:11:49 +0100

    First commit

---
+++ src/test/java/org/thoughtworks/FizzBuzzTest.java
@@ -0,0 +1,12 @@
+package org.thoughtworks;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FizzBuzzTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
